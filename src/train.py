0968a641c214c0e94c30d909babca4399c890e47
Austin Davis <austindavis@gmail.com>
2025-04-04 11:06:08 -0400

    Set val_check_interval to 1.0 for IterableDataset compatibility

--- src/train.py
+++ src/train.py
@@ -37,7 +37,7 @@ def main():
         limit_test_batches=0,
         # limit_val_batches=0,
         # max_epochs=-1,
-        val_check_interval=256,  # When using an IterableDataset you must set the val_check_interval to 1.0
+        val_check_interval=1.0,  # When using an IterableDataset you must set the val_check_interval to 1.0
         callbacks=[
             NtfyCallback(os.environ.get("NTFY_TOPIC", None)),
         ],
