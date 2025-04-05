4a91b677f457506fd1c2266431f5a09ab1de9f90
Austin Davis <austindavis@gmail.com>
2025-04-04 23:49:50 -0400

    Temporarily disable model training in train.py and add data module setup

--- src/train.py
+++ src/train.py
@@ -70,7 +70,9 @@ def main():
     ntfy = Ntfy(topic=os.environ.get("NTFY_TOPIC", None))
     ntfy.send_notification(f"Tuner Finished. {initial_lr.suggestion()=} {batch_size=}")
 
-    trainer.fit(model=sae, datamodule=dm)
+    dm.setup()
+    exit()
+    # trainer.fit(model=sae, datamodule=dm)
 
 
 if __name__ == "__main__":
