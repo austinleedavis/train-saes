87b4873697cf4a7e0096d58c15ce2590dabde17a
Austin Davis <austindavis@gmail.com>
2025-04-07 12:57:31 -0400

    Re-enabled callbacks

--- src/train.py
+++ src/train.py
@@ -40,26 +40,26 @@ def main():
         reload_dataloaders_every_n_epochs=1,
         limit_test_batches=0,
         # limit_val_batches=0,
-        max_epochs=10,
+        max_epochs=7,
         val_check_interval=1.0,  # When using an IterableDataset you must set the val_check_interval to 1.0
-        # callbacks=[
-        #     NtfyCallback(os.environ.get("NTFY_TOPIC", None)),
-        #     ModelCheckpoint(
-        #         dirpath="models",
-        #         monitor="val_loss",  # metric to monitor
-        #         mode="min",  # minimize val_acc
-        #         save_top_k=2,  # keep only top 2 checkpoints
-        #         save_last=True,  # also save the last checkpoint
-        #         every_n_train_steps=10_000,  # checkpoint every 1000 training steps
-        #         filename="epoch-{epoch}-step-{step}-{val_loss:.4f}",  # custom filename
-        #     ),
-        # ],
-        # logger=WandbLogger(
-        #     name="test",
-        #     project="TrainSae",
-        #     log_model=False,
-        #     checkpoint_name=None,
-        # ),
+        callbacks=[
+            NtfyCallback(os.environ.get("NTFY_TOPIC", None)),
+            ModelCheckpoint(
+                dirpath="models",
+                monitor="val_loss",  # metric to monitor
+                mode="min",  # minimize val_acc
+                save_top_k=2,  # keep only top 2 checkpoints
+                save_last=True,  # also save the last checkpoint
+                every_n_epochs=1,  # checkpoint every 1000 training steps
+                filename="epoch-{epoch}-step-{step}-{val_loss:.4f}",  # custom filename
+            ),
+        ],
+        logger=WandbLogger(
+            name="test",
+            project="TrainSae",
+            log_model=False,
+            checkpoint_name=None,
+        ),
     )
 
     # tuner = Tuner(trainer)
