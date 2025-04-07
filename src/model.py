c620d8d628ce8aab02620f624cd315744eeda068
Austin Davis <austindavis@gmail.com>
2025-04-07 12:42:54 -0400

    removed debug prints

--- src/model.py
+++ src/model.py
@@ -157,9 +157,6 @@ class SparseCrosscoder(L.LightningModule):
         :type attention_mask_BLPD: torch.Tensor
 
         """
-        print(f"{model_activations_BLPD.shape=}")
-        print(f"{attention_mask_BLPD.shape=}")
-        exit()
         encoded_representation_BPF = self.encode(model_activations_BLPD)
         reconstructed_model_activations_BLPD = self.decode(encoded_representation_BPF)
 
