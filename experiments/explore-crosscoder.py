5a5e20c6b92a87c10a160bcda9f7bd8c07466a13
Austin Davis <austindavis@gmail.com>
2025-04-11 08:49:15 -0400

    Applied Black Formatter

--- experiments/explore-crosscoder.py
+++ experiments/explore-crosscoder.py
@@ -11,20 +11,28 @@ from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
 from src.model import SparseCrosscoder
 from src.utils.chess_utils import uci_to_board, uci_to_pgn
 
-DEVICE = "cuda"
+PROBE_DEVICE = "cuda"
+LLM_DEVICE = "cuda"
 # %%
 
-tok: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained("austindavis/chessGPT2")
-llm: GPT2LMHeadModel = (
-    GPT2LMHeadModel.from_pretrained("austindavis/chessGPT2").to(DEVICE).train(False).requires_grad_(False)
+tok: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained(
+    "austindavis/chessGPT2"
 )
-scc: SparseCrosscoder = (
-    SparseCrosscoder.load_from_checkpoint("models/last-v2.ckpt")
-    .to(DEVICE)
+llm: GPT2LMHeadModel = (
+    GPT2LMHeadModel.from_pretrained("austindavis/chessGPT2")
     .train(False)
     .requires_grad_(False)
+    .to(LLM_DEVICE)
 )
-ds: datasets.Dataset = datasets.load_dataset("austindavis/lichess-uci", "201301", split="train")
+scc: SparseCrosscoder = (
+    SparseCrosscoder.from_pretrained("models/last-v3-state_dict.pt")
+    .train(False)
+    .requires_grad_(False)
+    .to(PROBE_DEVICE)
+)
+ds: datasets.Dataset = datasets.load_dataset(
+    "austindavis/lichess-uci", "201301", split="train"
+)
 
 
 # %%
@@ -41,7 +49,7 @@ def board_to_piece_list(board: chess.Board):
     return torch.tensor(piece_list, dtype=torch.long)
 
 
-def collate(record):
+def collate(record: dict, max_tokens: int = 1 + 3 * 40):
     # compute hidden states
     transcript = record["Transcript"]
     text_encoding = tok.encode_plus(
@@ -50,19 +58,24 @@ def collate(record):
         return_length=True,
         return_token_type_ids=False,
         return_attention_mask=False,
-    ).to(DEVICE)
+        max_length=max_tokens,
+        truncation=True,
+    ).to(LLM_DEVICE)
     *_, hidden_states = llm.forward(**text_encoding, output_hidden_states=True).values()
-    stacked_hidden_states = torch.stack(hidden_states[1:], dim=1)
+    stacked_hidden_states = torch.stack(hidden_states[1:], dim=1).to(PROBE_DEVICE)
 
     # compute board states
-    board_stack = uci_to_board(transcript, as_board_stack=True, map_function=board_to_piece_list)
+    board_stack = uci_to_board(
+        transcript, as_board_stack=True, map_function=board_to_piece_list
+    )
     length = text_encoding.length.item()
     board_indices = [0] + [i // 3 for i in range(length - 1)]
     boards = [board_stack[i] for i in board_indices]
+    boards = torch.stack(boards, dim=1).to(PROBE_DEVICE)
     return dict(
         model_activations_BLPD=stacked_hidden_states,
         transcript=transcript,
-        boards=torch.stack(boards, dim=1).to(DEVICE),
+        boards=boards,
         text_encoding=text_encoding,
     )
 
@@ -74,25 +87,27 @@ def collate(record):
 ############
 
 # Use this line if training from scratch:
-probe = torch.nn.ModuleList([torch.nn.Linear(scc.dict_size, len(stoi), bias=True) for _ in range(64)]).to(
-    DEVICE
-)
+# probe = torch.nn.ModuleList(
+# [torch.nn.Linear(scc.dict_size, len(stoi), bias=True) for _ in range(64)]
+# ).to(PROBE_DEVICE)
 
 # Use this block if loading from checkpoint:
-CKPT_TO_LOAD = "models/probe-step-606659-loss-0.37444.pt"
+CKPT_TO_LOAD = "models/probe-last-v3/probe-step-481995-loss-0.46329.pt"
 ckpt = torch.load(CKPT_TO_LOAD, weights_only=False)
-probe = ckpt["probe"]
+probe: torch.nn.ModuleList = ckpt["probe"]
 
 print(probe)
 
 
 # %%
-def save_checkpoint(epoch, ds, step, losses, last_checkpoint_index, checkpoint_files, probe):
+def save_checkpoint(
+    epoch, ds, step, losses, last_checkpoint_index, checkpoint_files, save_folder, probe
+):
     global_step = len(ds) * epoch + step
     loss_val = "NA"
     if losses:
         loss_val = f"{losses[-1]['avg_loss']:.5f}"
-    filename = f"models/probe-step-{global_step}-loss-{loss_val}.pt"
+    filename = os.path.join(save_folder, f"probe-step-{global_step}-loss-{loss_val}.pt")
 
     # cycle the checkpoint files so save only up to NUM_CHECKPOINTS
     this_checkpoint_index = (last_checkpoint_index + 1) % NUM_CHECKPOINTS
@@ -116,17 +131,23 @@ NUM_CHECKPOINTS = 3
 POS_OFFSET = 0
 POS_STEP_SIZE = 3
 POS_COUNT = 24
-NUM_EPOCHS = 5
-LOG_FILE = "models/probe.log"
+NUM_EPOCHS = 1
+MODEL_PATH = "models/probe-last-v3"
+LOG_FILE = os.path.join(MODEL_PATH, "probe.log")
+
+os.makedirs(MODEL_PATH, exist_ok=True)
 
 class_counts = {"p": 8, "n": 2, "b": 2, "r": 2, "q": 1, "k": 1, "none": 16}
 class_weights = torch.tensor(
-    [1.0 / class_counts[str(c[0]).lower()] for c in sorted(stoi.items(), key=lambda x: x[1])]
+    [
+        1.0 / class_counts[str(c[0]).lower()]
+        for c in sorted(stoi.items(), key=lambda x: x[1])
+    ]
 )
 class_weights = class_weights / class_weights.sum()
-loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights.to(DEVICE))
+loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights.to(PROBE_DEVICE))
 
-optimizer = torch.optim.AdamW(probe.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(probe.parameters(), lr=0.00011371)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
     optimizer, NUM_EPOCHS * len(ds) // GRADIENT_ACCUM_STEPS
 )
@@ -141,15 +162,22 @@ with open(LOG_FILE, "a") as log:
         ds = ds.shuffle()
         optimizer.zero_grad()
         running_loss = 0
-        for step, record in enumerate(tqdm(ds, desc="Step", leave=False)):
+        for step, record in enumerate(tqdm(ds, desc="Step", leave=True)):
             if len(record["Transcript"]) < min_characters:
                 continue
 
-            model_activations_BLPD, transcript, boards, text_encoding = collate(record).values()
-            features = scc.encode(model_activations_BLPD).squeeze()  # eliminate batch dim
+            model_activations_BLPD, transcript, boards, text_encoding = collate(
+                record
+            ).values()
+            features = scc.encode(
+                model_activations_BLPD
+            ).squeeze()  # eliminate batch dim
+            del model_activations_BLPD  # free that memory fast
 
             predictions = torch.stack([p(features) for p in probe]).permute(0, 2, 1)
-            selected_predictions = predictions[:, :, -POS_COUNT:][:, :, POS_OFFSET::POS_STEP_SIZE]
+            selected_predictions = predictions[:, :, -POS_COUNT:][
+                :, :, POS_OFFSET::POS_STEP_SIZE
+            ]
             selected_boards = boards[:, -POS_COUNT:][:, POS_OFFSET::POS_STEP_SIZE]
             loss = loss_fn(selected_predictions, selected_boards)
             loss.backward()
@@ -165,7 +193,14 @@ with open(LOG_FILE, "a") as log:
                 avg_loss = running_loss / LOG_INTERVAL
                 running_loss = 0
                 global_step = len(ds) * epoch + step
-                losses.append(dict(epoch=epoch, step=step, global_step=global_step, avg_loss=avg_loss))
+                losses.append(
+                    dict(
+                        epoch=epoch,
+                        step=step,
+                        global_step=global_step,
+                        avg_loss=avg_loss,
+                    )
+                )
                 log.writelines(
                     [
                         f"|  Epoch: {1+epoch: 3}  |  Step: {1+step: 8,}  |  Average Loss: {avg_loss:.5f}  |  LR: {lr_scheduler.get_last_lr()[-1]:.8f}  |\n"
@@ -175,10 +210,17 @@ with open(LOG_FILE, "a") as log:
 
             if (step + 1) % CHECKPOINT_INTERVAL == 0:
                 last_checkpoint_index = save_checkpoint(
-                    epoch, ds, step, losses, last_checkpoint_index, checkpoint_files, probe
+                    epoch,
+                    ds,
+                    step,
+                    losses,
+                    last_checkpoint_index,
+                    checkpoint_files,
+                    MODEL_PATH,
+                    probe,
                 )
 
-
+exit(1)
 # %%
 px.line(
     x=[d["global_step"] for d in losses],
@@ -252,7 +294,9 @@ def label(val):
 
 pred = predictions.detach().to("cpu").squeeze()
 frame_tensor = (
-    pred.max(1, keepdim=True).indices.view(8, 8, 1, -1)[:, :, :, POS_OFFSET::POS_STEP_SIZE].flip(0)
+    pred.max(1, keepdim=True)
+    .indices.view(8, 8, 1, -1)[:, :, :, POS_OFFSET::POS_STEP_SIZE]
+    .flip(0)
 )
 
 fig = px.imshow(
